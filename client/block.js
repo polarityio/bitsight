218912a30a346f58dac943534ba240b6b6d8cf9d
Ed <ed@polarity.io>
2023-05-26 11:34:42 -0400

    Ensure state is not shared between entities

--- client/block.js
+++ client/block.js
@@ -3,38 +3,61 @@ polarity.export = PolarityComponent.extend({
   timezone: Ember.computed('Intl', function () {
     return Intl.DateTimeFormat().resolvedOptions().timeZone;
   }),
-  getCompanyInsightsErrorMessage: {},
-  getCompanyInsightsIsRunning: {},
-  expandableTitleStates: {},
-  companyInsightsStates: {},
-  highestRatingInsightsStates: {},
-  lowestRatingInsightsStates: {},
+  getCompanyInsightsErrorMessage: Ember.computed.alias(
+    'block._state.getCompanyInsightsErrorMessage'
+  ),
+  getCompanyInsightsIsRunning: Ember.computed.alias(
+    'block._state.getCompanyInsightsIsRunning'
+  ),
+  expandableTitleStates: Ember.computed.alias('block._state.expandableTitleStates'),
+  companyInsightsStates: Ember.computed.alias('block._state.companyInsightsStates'),
+  highestRatingInsightsStates: Ember.computed.alias(
+    'block._state.highestRatingInsightsStates'
+  ),
+  lowestRatingInsightsStates: Ember.computed.alias(
+    'block._state.lowestRatingInsightsStates'
+  ),
+  init: function () {
+    if (!this.get('block._state')) {
+      this.set('block._state', {});
+      this.set('block._state.getCompanyInsightsErrorMessage', {});
+      this.set('block._state.getCompanyInsightsIsRunning', {});
+      this.set('block._state.expandableTitleStates', {});
+      this.set('block._state.companyInsightsStates', {});
+      this.set('block._state.highestRatingInsightsStates', {});
+      this.set('block._state.lowestRatingInsightsStates', {});
+    }
+    this._super(...arguments);
+  },
   getCompanyInsights: function (index, companyGuid) {
-    this.set(`getCompanyInsightsErrorMessage.${index}`, '');
-    this.set(`getCompanyInsightsIsRunning.${index}`, true);
+    this.set(`block._state.getCompanyInsightsErrorMessage.${index}`, '');
+    this.set(`block._state.getCompanyInsightsIsRunning.${index}`, true);
 
     this.sendIntegrationMessage({ action: 'getCompanyInsights', data: { companyGuid } })
       .then(({ companyInsights, highestRating, lowestRating }) => {
-        this.set(`companyInsightsStates.${index}`, companyInsights);
-        this.set(`expandableTitleStates.${index}`, true);
-        this.set(`highestRatingInsightsStates.${index}`, highestRating);
-        this.set(`lowestRatingInsightsStates.${index}`, lowestRating);
+        this.set(`block._state.companyInsightsStates.${index}`, companyInsights);
+        this.set(`block._state.expandableTitleStates.${index}`, true);
+        this.set(`block._state.highestRatingInsightsStates.${index}`, highestRating);
+        this.set(`block._state.lowestRatingInsightsStates.${index}`, lowestRating);
       })
       .catch((err) => {
         this.set(
-          `getCompanyInsightsErrorMessage.${index}`,
+          `block._state.getCompanyInsightsErrorMessage.${index}`,
           (err &&
             (err.detail || err.err || err.message || err.title || err.description)) ||
             'Unknown Reason'
         );
       })
       .finally(() => {
-        this.set(`getCompanyInsightsIsRunning.${index}`, false);
+        this.set(`block._state.getCompanyInsightsIsRunning.${index}`, false);
 
         setTimeout(() => {
-          if (!this.isDestroyed && this.get(`getCompanyInsightsErrorMessage.${index}`)) {
-            this.set(`getCompanyInsightsErrorMessage.${index}`, '');
-            this.set(`expandableTitleStates.${index}`, false);
+          if (
+            !this.isDestroyed &&
+            this.get(`block._state.getCompanyInsightsErrorMessage.${index}`)
+          ) {
+            this.set(`block._state.getCompanyInsightsErrorMessage.${index}`, '');
+            this.set(`block._state.expandableTitleStates.${index}`, false);
           }
         }, 5000);
       });
@@ -42,10 +65,10 @@ polarity.export = PolarityComponent.extend({
   actions: {
     toggleExpandableTitle: function (index, companyGuid) {
       this.set(
-        `expandableTitleStates.${index}`,
-        !this.get(`expandableTitleStates.${index}`)
+        `block._state.expandableTitleStates.${index}`,
+        !this.get(`block._state.expandableTitleStates.${index}`)
       );
-      if (!this.get('companyInsightsStates')[index])
+      if (!this.get('block._state.companyInsightsStates')[index])
         this.getCompanyInsights(index, companyGuid);
     }
   }
